aafe091a0d71862802f216422d8a1ad797cf1642
tanatswatech <tanatswatech@gmail.com>
2026-05-06 13:06:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -118,3 +118,23 @@ function loginSeller() {
     alert("Invalid details");
   }
 }
+function addProduct() {
+  const name = document.getElementById("pName").value;
+  const price = document.getElementById("pPrice").value;
+  const image = document.getElementById("pImage").value;
+
+  if (!name || !price || !image) {
+    alert("Fill all fields");
+    return;
+  }
+
+  const product = { name, price, image };
+
+  let products = JSON.parse(localStorage.getItem("products")) || [];
+
+  products.push(product);
+
+  localStorage.setItem("products", JSON.stringify(products));
+
+  alert("Product added!");
+}
