d84af4bc4476500662c1f0a9cafd9bd6f05275ec
tanatswatech <tanatswatech@gmail.com>
2026-05-06 12:27:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ function buyProduct() {
     alert("Next: Register/Login page");
   }, 1200);
 }
+function registerUser() {
+  const name = document.getElementById("name").value;
+  const surname = document.getElementById("surname").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (!name || !surname || !email || !password) {
+    alert("Please fill all fields");
+    return;
+  }
+
+  const user = {
+    name,
+    surname,
+    email,
+    password
+  };
+
+  // SAVE USER (LOCAL STORAGE)
+  localStorage.setItem("user", JSON.stringify(user));
+
+  alert("Registered successfully!");
+
+  // GO TO CHECKOUT (next step)
+  window.location.href = "checkout.html";
+}
